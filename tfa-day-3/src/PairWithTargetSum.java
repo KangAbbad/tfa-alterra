07651f1bb79549737f575a104c6b0f6504ce20a7
KangAbbad <naufalabbad9@gmail.com>
2022-08-18 17:03:59 +0700

    feature: task pair with target sum

--- tfa-day-3/src/PairWithTargetSum.java
+++ tfa-day-3/src/PairWithTargetSum.java
@@ -1,2 +1,28 @@
+import java.util.*;
+
 public class PairWithTargetSum {
+  public static void main(String[] args) {
+    System.out.print("Input Numbers: ");
+    Scanner inputArray1 = new Scanner(System.in);
+    String array = inputArray1.nextLine();
+
+    System.out.print("Input Target: ");
+    Scanner inputTarget = new Scanner(System.in);
+    int target = inputTarget.nextInt();
+
+    List<String> numbers = Arrays.asList(array.split("\\s*,\\s*"));
+    List<Integer> indexes = new ArrayList<>();
+
+    for (int i = 0; i < numbers.size(); i++) {
+      int numI = Integer.parseInt(numbers.get(i));
+      for (int j = 0; j < numbers.size(); j++) {
+        int numJ = Integer.parseInt(numbers.get(j));
+        if (numI != numJ && numI + numJ == target) {
+          indexes.add(i);
+        }
+      }
+    }
+
+    System.out.println("Output: " + indexes);
+  }
 }
