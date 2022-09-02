f8ab5aea64f6b86fd9f51aa6fe24c64449a64783
KangAbbad <naufalabbad9@gmail.com>
2022-09-02 19:26:25 +0700

    feature: add new folder

--- tfa-day-12/chatapp-remake/src/main/java/com/alta/bootcamp/chatappremake/ChatappRemakeApplication.java
+++ tfa-day-12/chatapp-remake/src/main/java/com/alta/bootcamp/chatappremake/ChatappRemakeApplication.java
@@ -2,12 +2,11 @@ package com.alta.bootcamp.chatappremake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.*;
 
 @SpringBootApplication
 public class ChatappRemakeApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(ChatappRemakeApplication.class, args);
   }
-
 }
