8e89fb5b3a5d0027b842c2661332025b5b25be3f
KangAbbad <naufalabbad9@gmail.com>
2022-10-10 12:42:00 +0700

    fix: task day 33 - unit test

--- fe-day-13/passenger-website/src/app/pages/auth/login/login.component.spec.ts
+++ fe-day-13/passenger-website/src/app/pages/auth/login/login.component.spec.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { LoginComponent } from './login.component';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -25,5 +26,53 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should invalid when forms are empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('should valid email', () => {
+    component.loginForm.setValue({
+      email: 'someone@email.com',
+      password: '',
+    });
+    expect(component.loginForm.controls['email'].errors).toBeFalsy();
+  });
+
+  it('should valid password', () => {
+    component.loginForm.setValue({
+      email: '',
+      password: '123123',
+    });
+    expect(component.loginForm.controls['password'].errors).toBeFalsy();
+  });
+
+  it('should valid form', () => {
+    const validForm = {
+      email: 'someone@email.com',
+      password: '123123',
+    };
+    component.loginForm.setValue(validForm);
+    expect(component.loginForm.value).toEqual(validForm);
+  });
+
+  it('should submit form when submit button is clicked', () => {
+    const submitBtnElm = fixture.debugElement.query(By.css('button[type=submit]'));
+    const submitFunc = spyOn(component, 'onSubmit');
+
+    (submitBtnElm.nativeElement as HTMLButtonElement).click();
+    fixture.detectChanges();
+
+    expect(submitFunc).toHaveBeenCalled();
+  });
+
+  it('should call onSubmit method', () => {
+    const formElm = fixture.debugElement.query(By.css('form'));
+    const submitFunc = spyOn(component, 'onSubmit');
+
+    formElm.triggerEventHandler('ngSubmit', null);
+
+    expect(submitFunc).toHaveBeenCalled();
   });
 });
--- fe-day-13/passenger-website/src/app/pages/homepage/homepage.component.spec.ts
+++ fe-day-13/passenger-website/src/app/pages/homepage/homepage.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HomepageComponent } from './homepage.component';
 
@@ -8,7 +9,8 @@ describe('HomepageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [ HomepageComponent ],
+      imports: [NgbModule],
     })
     .compileComponents();
 
